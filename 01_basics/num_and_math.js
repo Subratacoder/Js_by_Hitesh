68dfa8ae097783dc8a9bd621e10748ca6b738fc1
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2023-08-24 04:13:20 +0000

    Enter into Math sec

--- 01_basics/num_and_math.js
+++ 01_basics/num_and_math.js
@@ -1,9 +1,20 @@
-const score = 500
-console.log(score);
+// const score = 500
+// console.log(score);
 
-const balance = new Number(100)
-console.log(balance); 
+// const balance = new Number(100)
+// console.log(balance); 
 
 // All these are methods in bellow
 
-console.log(balance.toString())
+// console.log(balance.toString());
+// console.log(balance.toFixed());
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// *******************Maths*****************
+
+console.log(Math);
