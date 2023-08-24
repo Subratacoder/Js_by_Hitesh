dd8d15453a5f1b30994831f2712f122f34b1e2d4
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2023-08-24 18:43:21 +0000

    Math.js Updated

--- 01_basics/num_and_math.js
+++ 01_basics/num_and_math.js
@@ -18,3 +18,11 @@
 // *******************Maths*****************
 
 console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+console.log(Math.random());
+console.log(Math.random());
