8b9a3281e5d5a32d44f0b467c6a1a4cff6922315
Subrata Acharjee <rp6398812@gmail.com>
2023-10-28 00:46:55 +0100

    Update in 27 oct

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,6 +1,13 @@
 // array
 
-const newArr = [0, 1, 2, 3, 4, 5]
+const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["Batman", "Superman", "Flash"]
+// For change array element
+myHeros[2] = "Spiderman";
+// For add array element
+console.log(myHeros);
 
- console.log(myArr[0]);
+myHeros[3] = "Flash";
+console.log(myHeros);
+
+console.log(myArr[0]);
