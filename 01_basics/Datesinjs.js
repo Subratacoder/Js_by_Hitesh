b2be37ab562119bb2c7a88e0e99e80559f160feb
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-01 05:16:46 +0000

    Changes in file

--- 01_basics/Datesinjs.js
+++ 01_basics/Datesinjs.js
@@ -2,4 +2,6 @@
 
 let myDate = new Date( )
 console.log(myDate.toString());
-console.log(myDate);
+// console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
