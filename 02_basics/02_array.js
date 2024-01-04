230223d07fcf2c0511a84494ce4b0f858def0157
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-04 17:55:28 +0000

    Code added

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,6 +1,19 @@
 const marvel_heroes = ["Ironman", "Spiderman", "Thor"]
 const dc_heroes = ["Batman", "Superman", "Flash"]
 
-marvel_heroes.push(dc_heroes)
+// marvel_heroes.push(dc_heroes)
 
-console.log(marvel_heroes); 
+// console.log(marvel_heroes); 
+// console.log(marvel_heroes[3][0]); 
+
+// allHeroes = marvel_heroes.concat(dc_heroes);
+// console.log(allHeroes);
+
+//Spread Operator
+
+const my_all_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(my_all_heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [8, 9, [1, 0 ]]];
+const new_another_array = another_array.flat(2);
+console.log(new_another_array);
