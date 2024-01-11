832c5d7a2bff935b0484319472e899200659d078
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-11 13:12:01 +0000

    Code added in file

---
+++ 02_basics/object.js
@@ -0,0 +1,10 @@
+// Singleton
+
+// Object Literal
+// Object.create // Construtor method
+const user = {
+    name: "Subrata",
+    age: 20,
+    location: "Kolkata",
+    email_Id: "subrataacharjee39@gmail.com",
+}
