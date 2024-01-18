3bf6d33bb88be08e6018d86e089f9e818b860d42
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-18 18:53:00 +0000

    Code updated

--- 02_basics/object2.js
+++ 02_basics/object2.js
@@ -1,3 +1,29 @@
-const xuser = new Object() // singleton object
+// const xuser = new Object() // singleton object
+const fbuser = {}
 
-console.log(xuser);
+fbuser.id = "123Subho"
+fbuser.name = "Subrata"
+fbuser.isloggedin = false
+
+// console.log(fbuser);
+console.log(Object.keys(fbuser));
+console.log(Object.values(fbuser));
+
+const anotheruser = {
+    email: "subrata@chatgpt",
+    username: {
+        fullname: {
+            firstname: "Subrata",
+            lastname: "Acharjee",
+        }
+    }
+}
+
+// console.log(anotheruser.username);
+// console.log(anotheruser.username.fullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+// const newobj = Object.assign({},obj1, obj2)
+const newobj = {...obj1,...obj2}
+// console.log(newobj);
