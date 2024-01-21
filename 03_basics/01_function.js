cdfdae162ac9905704e1e1a8399e85a9a63364a2
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-21 18:40:17 +0000

    creat function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,11 @@
+function name(){
+    console.log("S");
+    console.log("U");
+    console.log("B");
+    console.log("R");
+    console.log("A");
+    console.log("T");
+    console.log("A");
+}
+
+name ()
