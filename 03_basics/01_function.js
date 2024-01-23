910c4443af6365da601d690f45a0a6a7751e48fb
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-01-23 18:42:51 +0000

    unfinished code

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -12,10 +12,20 @@
 
 function addTwoNumbers(num1, num2) {
     // console.log(num1 + num2);
-    let sumoftwo = num1 + num2
-    return sumoftwo
+    // let sumoftwo = num1 + num2
+    // return sumoftwo
+    return num1 + num2
 }
 
 const sumoftwo = addTwoNumbers(20, 20)
+// console.log("sumoftwo: ", sumoftwo);
 
-console.log("sumoftwo: ", sumoftwo);
+function loginUsserMessage(username) {
+    if (username == undefined) {
+        console.log("Please enter username");
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUsserMessage("Subrata"));
+console.log(loginUsserMessage());
