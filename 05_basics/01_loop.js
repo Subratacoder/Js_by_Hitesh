e0fcf709877b5063039248743207320424805e5c
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-05-16 16:49:54 +0000

    code added

--- 05_basics/01_loop.js
+++ 05_basics/01_loop.js
@@ -1,6 +1,42 @@
-// for 
+// // for loop
 
-for (let index = 0; index < 10; index++) {
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if (element == 5) {
+//         // console.log("Here is your Number 🙏");
+//     }
+//     // console.log(element);
+// }
+
+// for (let index = 0; index <= 10; index++) {
+//     // console.log(`outer loop : ${index}`);
+//     for (let i = 0; i <= 10; i++) {
+//     //    console.log(`inner loop value ${i} & inner loop ${index} `);
+        
+//     }
+// }
+
+// let myarr = ["Ironman", "Thor", "Loki", "Captain"]
+// for (let index = 0; index < myarr.length; index++) {
+//     const element = myarr[index];
+//     console.log(element);
+// }
+
+// break & continue
+
+// for (let index = 1; index <= 20; index++) {
+//     const element = index;
+//     if (index == 5) {
+//         console.log("Detected 5");
+//         break
+//     }
+//     console.log(element);
+// }
+for (let index = 1; index <= 20; index++) {
     const element = index;
+    if (index == 5) {
+        console.log("Detected 5");
+        continue
+    }
     console.log(element);
 }
