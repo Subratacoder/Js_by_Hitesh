275cfd467a08c8571e365e5b619d2a7b87f9f8dd
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-05-16 18:31:50 +0000

    File added

---
+++ 05_basics/02_loop.js
@@ -0,0 +1,23 @@
+// while & do while loop
+
+// While loop
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index ${index}`);
+    index = index + 2
+}
+
+let myarr = ["Batman", "Superman", "Loki"]
+let arr = 0
+while (arr < myarr.length) {
+    // console.log(`value is ${myarr[arr]}`);
+    arr ++
+}
+
+// Do while loop
+
+let level = 11
+do {
+    console.log(`level is ${level}`);
+    level++
+} while (level <= 10);
---
+++ 05_basics/03_loop.js
@@ -0,0 +1,10 @@
+// for of loop
+
+const myHeros = ["Krish", "Saktiman", "Hanuman"]
+for (const index of myHeros) {
+//    console.log(index); 
+}
+
+// Maps
+
+const map = new map()
