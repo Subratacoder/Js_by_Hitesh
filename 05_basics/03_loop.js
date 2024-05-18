69069887f7d865fac2c38f31e48174a196777471
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-05-18 18:18:16 +0000

    Update in file

--- 05_basics/03_loop.js
+++ 05_basics/03_loop.js
@@ -1,10 +1,16 @@
 // for of loop
 
-const myHeros = ["Krish", "Saktiman", "Hanuman"]
-for (const index of myHeros) {
+// const myHeros = ["Krish", "Saktiman", "Hanuman"]
+// for (const index of myHeros) {
 //    console.log(index); 
-}
+// }
 
 // Maps
 
 const map = new map()
+map.set('IN', "India")
+map.set('EN', "England")
+map.set('Jap', "Japan")
+map.set('PAK', "Pakistan")
+
+console.log(map);
