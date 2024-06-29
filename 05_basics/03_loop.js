caa9ad201a39f027eecef51dcad1f1e1d8e464fb
Subrata Acharjee <141865409+Subratacoder@users.noreply.github.com>
2024-06-29 16:30:51 +0000

    Change in file

--- 05_basics/03_loop.js
+++ 05_basics/03_loop.js
@@ -7,10 +7,23 @@
 
 // Maps
 
-const map = new map()
+const map = new Map()
 map.set('IN', "India")
 map.set('EN', "England")
 map.set('Jap', "Japan")
 map.set('PAK', "Pakistan")
 
-console.log(map);
+// console.log(map);
+
+for (const [key, val] of map) {
+    // console.log(`${key} == ${val}`);
+}
+
+const myObject = {
+    'Game1' : 'GTA-5',
+    'Game2' : 'NFS',
+}
+
+for (const Game of myObject) {
+    console.log(Game);
+}
